d2bf8f88f77ccb6baa90a2b5f2e32f9f0d362704
Mykon89 <helvis89@gmail.com>
2022-06-06 14:39:13 -0300

    advinha_numero

---
+++ PYTHON/advinha_numero.py
@@ -0,0 +1,8 @@
+import random
+import math
+
+min = int(input("Informe o menor número: "))
+max = int(input("Informe o maior número: "))
+
+x = random.randint(min, max)
+print("\n\tVocê tem somente {}")
